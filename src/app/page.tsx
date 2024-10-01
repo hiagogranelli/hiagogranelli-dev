75d42aa991948ea4d941fd89915413eb3c25824f
Hiago Granelli <hiagogranelli12@gmail.com>
2024-10-01 19:17:52 -0300

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>hi, I&apos;m Hiago</h1>
+      <div className="flex gap-4">
+        <Link href="https://www.linkedin.com/in/hiagogranelli/" target="_blank">
+          <FaLinkedinIn />
+        </Link>
+        <Link href="https://github.com/hiagogranelli" target="_blank">
+          <FaGithub />
+        </Link>
+      </div>
     </div>
   );
 }
